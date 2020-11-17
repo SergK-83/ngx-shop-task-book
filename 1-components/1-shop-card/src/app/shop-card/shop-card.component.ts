e07a67156f9842a79cca52f863eee9366c5c6596
Sergey Kryukov <krus-83@mail.ru>
2020-11-17 21:01:36 +0700

    shop-card task

--- 1-components/1-shop-card/src/app/shop-card/shop-card.component.ts
+++ 1-components/1-shop-card/src/app/shop-card/shop-card.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {ICartProduct} from '../../../../../shared/mocks/1-components/cart-product';
 
 @Component({
   selector: 'app-shop-card',
   templateUrl: './shop-card.component.html',
 })
 
-export class ShopCardComponent {
+export class ShopCardComponent implements OnInit{
+  @Input()
+  public product: ICartProduct = {} as ICartProduct;
+
+  @Output()
+  public decrement = new EventEmitter();
+
+  @Output()
+  public increment = new EventEmitter();
+
+  public stars: number[] = [1, 2, 3, 4, 5];
+
+  ngOnInit() {
+  }
+
+  public decrementProductInCart(): void {
+    this.decrement.emit();
+  }
+
+  public incrementProductInCart(): void {
+    this.increment.emit();
+  }
 
 }
